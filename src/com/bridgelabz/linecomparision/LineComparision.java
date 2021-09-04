40d540398f36aa066af1791eedfcaaec2529c4a2
srinadh1304 <srinadh1304@gmail.com>
2021-09-04 13:13:55 +0530

    ADDED: Calculating length of line using two points

--- src/com/bridgelabz/linecomparision/LineComparision.java
+++ src/com/bridgelabz/linecomparision/LineComparision.java
@@ -3,5 +3,12 @@ package com.bridgelabz.linecomparision;
 public class LineComparision {
 	public static void main(String[] args) {
 		System.out.println("****** Welcome to Line Comparision Program ******");
+		int point1X1=0,point1Y1=0,point2X2=0,point2Y2=0;
+		point1X1=(int)Math.floor(Math.random()*10);
+		point1Y1=(int)Math.floor(Math.random()*10);
+		point2X2=(int)Math.floor(Math.random()*10);
+		point2Y2=(int)Math.floor(Math.random()*10);
+		double lengthOfLine=Math.sqrt(((point2X2-point1X1)*(point2X2-point1X1))+((point2Y2-point1Y1)*(point2Y2-point1Y1)));
+		System.out.println("Length of line: "+lengthOfLine);
 	}
 }
