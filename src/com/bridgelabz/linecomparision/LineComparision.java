802b75a0e4188445d4fed50e199e780ba0cf02e5
srinadh1304 <srinadh1304@gmail.com>
2021-09-04 13:24:12 +0530

    MODIFIED: Comparing lengths of two lines using compareTo Method

--- src/com/bridgelabz/linecomparision/LineComparision.java
+++ src/com/bridgelabz/linecomparision/LineComparision.java
@@ -16,13 +16,17 @@ public class LineComparision {
 		line2Y2=(int)Math.floor(Math.random()*10);
 		Double lengthOfLine1=Math.sqrt(((line1X2-line1X1)*(line1X2-line1X1))+((line1Y2-line1Y1)*(line1Y2-line1Y1)));
 		Double lengthOfLine2=Math.sqrt(((line2X2-line2X1)*(line2X2-line2X1))+((line2Y2-line2Y1)*(line2Y2-line2Y1)));
-		if(lengthOfLine1.equals(lengthOfLine2))
+		if(lengthOfLine1.compareTo(lengthOfLine2)==0)
 		{
 			System.out.println("Length of Line 1 is equal to Length of Line 2");
 		}
-		else
+		else if(lengthOfLine1.compareTo(lengthOfLine2)<0)
 		{
-			System.out.println("Length of Line1 is not equal to Length of Line 2");
+			System.out.println("Length of Line 1 is less than  Length of Line 2");
+		}
+		else 
+		{
+			System.out.println("Length of Line 1 is greater than  Length of Line 2");
 		}
 	}
 }
